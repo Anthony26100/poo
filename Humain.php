1e9992099e9d35516ef2ddbd1f988daa43f6ba47
Anthony26100 <anthony.bellier@le-campus-numerique.fr>
2023-01-05 11:09:02 +0100

    héritage

--- Humain.php
+++ Humain.php
@@ -1,46 +1,68 @@
 <?php
-var_dump(Humain::class);
+
+class Homme extends Humain
+{
+    public $force = 2;
+}
+
+class Femme extends Humain
+{
+
+}
+
 class Humain
 {
     public $taille = 175;
     public $nom;
+
+    public $force = 1;
 
     private $secret;
     public function __construct(
         string $nomDeFamille
     )
     {
-        echo "Je suis né.e \n";
+        #echo "Je suis né.e \n";
         echo $this->nom = $nomDeFamille."\n";
     }
 
     public function __destruct()
     {
-        echo "Je suis mort.e \n";
+        #echo "Je suis mort.e \n";
     }
 
     public function marcher()
     {
-        echo "Je marche";
+        echo "Je marche ".$this->nom."\n";
     }
 
+    // Taille +1
     public function maTaille()
     {
        echo $this->taille+1;
     }
 
-    function setSecret(string $secret)
+    // Getter Setter $secret phrase
+    public function setSecret(string $secret)
     {
         $this->secret = $secret;
     }
 
-    function getSecret()
+    public function getSecret()
     {
         return $this->secret;
     }
+
+
 }
-$constance = new Humain('nomdefamilleConstance');
-$marcelline = new Humain('nomdefamilleMarcelline');
-$constance->setSecret("Je suis la phrase secret : de Constance \n");
-echo $constance->getSecret();
-unset($constance);
+$constance = new Femme('CONSTANCE');
+$marcelline = new Femme('MARCELLINE');
+$adam = new Homme('ADAM');
+#$constance->setSecret("Je suis la phrase secret : de Constance \n");
+echo $adam->marcher();
+echo "La force de ".$marcelline->nom." est de : ".$marcelline->force."\n";
+echo "\n";
+echo "La force de ".$adam->nom." est de : ".$adam->force;
+echo "\n";
+
+
